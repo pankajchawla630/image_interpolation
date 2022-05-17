d1366f085ff1ecaa9e3d879dbfd8d2f2fe423bf5
Pankaj <pankajchawla630@gmail.com>
2022-05-17 18:35:38 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
+import React,{useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [userInfo, setuserInfo] = useState({
+    filepreview:null,
+    filepreview2:null,
+   });
+
+  const handleInputChange = (event) => {
+    setuserInfo({
+      ...userInfo,
+      filepreview:URL.createObjectURL(event.target.files[0]),
+    });
+  }
+
+  const handleInputChange2 = (event) => {
+    setuserInfo({
+      ...userInfo,
+      filepreview2:URL.createObjectURL(event.target.files[0]),
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mr-60">
+
+      <h3 className="text-white">Image Interpolation</h3>
+
+      <div className="formdesign">
+        <div className="form-row">
+          <label className="text-white">Select Image 1 :</label>
+          <input type="file" className="form-control" name="upload_image"  onChange={handleInputChange} />
+        </div>
+      </div>
+
+      {userInfo.filepreview !== null ?
+        <img className="previewimg"  src={userInfo.filepreview} alt="UploadImage" />
+      : null}
+
+      <div className="formdesign">
+        <div className="form-row">
+          <label className="text-white">Select Image 2 :</label>
+          <input type="file" className="form-control" name="upload_image"  onChange={handleInputChange2} />
+        </div>
+      </div>
+
+      {userInfo.filepreview2 !== null ?
+        <img className="previewimg"  src={userInfo.filepreview2} alt="UploadImage" />
+      : null}
+
     </div>
   );
 }
